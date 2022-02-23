fe80b0955c32bcee0f940da73ae8829a610834fb
hungnn88dn <hungnn88dn@gmail.com>
2022-02-23 09:17:00 +0700

    hungnn

--- C0721G2-livestock-farm-FE/src/app/component/cages/cages-routing.module.ts
+++ C0721G2-livestock-farm-FE/src/app/component/cages/cages-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {CagesListComponent} from './cages-list/cages-list.component';
+import {CagesEditComponent} from './cages-edit/cages-edit.component';
+import {CagesCreateComponent} from './cages-create/cages-create.component';
 
 
 const routes: Routes = [
   {
     path: 'cage/list', component: CagesListComponent
   },
+  {
+    path: 'cage/create', component: CagesCreateComponent
+  }
 ];
 
 @NgModule({
